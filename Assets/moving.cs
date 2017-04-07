30d5b3e737a8d530904d830e05be78c616bda236
KristianBerg <kristianberg.jobb@gmail.com>
2017-04-07 10:56:36 +0200

    script

---
+++ Assets/moving.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour {
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ball.transform.position += new Vector3(0, 0.5f, 0);
+	}
+}
